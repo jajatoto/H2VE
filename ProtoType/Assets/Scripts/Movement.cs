bcabc2228f74554dd8eab8e367416f580402f83f
Niforances <sungyun1126@gmail.com>
2018-12-25 11:10:58 +0900

    스크립트
    방향키, WASD 간단한 조작의 스크립트
    적 AI 스크립트

---
+++ ProtoType/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 Move = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        transform.Translate(Move*moveSpeed*Time.deltaTime);
+
+
+    }
+}
